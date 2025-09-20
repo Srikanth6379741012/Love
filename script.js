b1b6fd80c64f74ca48728b1fff9b3e63e8545199
Srikanth6379741012 <srikanth.s9399@gmail.com>
2025-09-20 16:02:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const response = document.getElementById("response");
 
-yesBtn.addEventListener("click", () => {
+noBtn.addEventListener("click", () => {
   response.textContent = "Yay! You just made me the happiest person alive! 💍❤️";
 });
 
-noBtn.addEventListener("mouseover", () => {
+yesBtn.addEventListener("mouseover", () => {
   // Button runs away when hovered
   const x = Math.random() * window.innerWidth * 0.6;
   const y = Math.random() * window.innerHeight * 0.6;
